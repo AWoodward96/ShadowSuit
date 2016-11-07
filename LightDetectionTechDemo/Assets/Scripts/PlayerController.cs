3e9b7b28ad17b57129fa03de7f6a5ed0fb6d0dd8
RireMakar <abellavia42@gmail.com>
2016-11-07 04:37:06 -0500

    Made Light Detection Work
    Lights turn red when they can see the player for now

--- LightDetectionTechDemo/Assets/Scripts/PlayerController.cs
+++ LightDetectionTechDemo/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public GameObject PointLight;
-    public GameObject SpotLight;
+    public GameObject[] pointLights;
+    public GameObject[] spotLights;
     private SpriteRenderer spriteRend;
 
     public bool lightDebug;
@@ -18,14 +18,21 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (lightDebug)
+        foreach (GameObject spotLight in spotLights)
         {
-            if (isLitSpot(SpotLight))
-                spriteRend.color = Color.blue;  // if in a point light, turns blue
-            else if (isLitPoint(PointLight))
-                spriteRend.color = Color.red;   // if in a spot light, turns red
-            else
-                spriteRend.color = Color.white; // if in neither, turns white
+            if (isLitSpot(spotLight))
+            {
+                spriteRend.color = Color.blue;
+                break;
+            }
+        }
+        foreach (GameObject pointLight in pointLights)
+        {
+            if (isLitPoint(pointLight))
+            {
+                spriteRend.color = Color.red;
+                break;
+            }
         }
 
         processInput();
@@ -37,16 +44,24 @@ public class PlayerController : MonoBehaviour {
         Vector3 lightToPlayer = this.transform.position - lite.GetComponent<Transform>().position;  // gets a vector from the light to the player
         Vector3 lightForward = lite.GetComponent<Transform>().forward;  // gets the forward vector of the light
         Vector3 lightFlattened = lite.GetComponent<Transform>().position;   // gets position vector of the light
-        lightForward.z = 0;     // flattens the forward vector on the z axis 
-        lightToPlayer.z = 0;    // flattens the vector to the player on the z axis
+        lightForward.y = 0;     // flattens the forward vector on the y axis 
+        lightToPlayer.y = 0;    // flattens the vector to the player on the y axis
         float arc = lt.spotAngle;   // gets the spot angle of the light
         float angle = Vector3.Angle(lightForward, lightToPlayer);   // gets the angle between the forward vector and the vector to the player
 
-        Debug.DrawRay(lightFlattened, lightToPlayer, Color.green);  // draws debug ray
-        if (Physics.Raycast(lightFlattened, lightToPlayer, lt.range * .4f) && angle < (arc/2))  // checks to see if it is close enough to the player to actually cast light AND if it is within the arc of light 
+        RaycastHit hit;
+
+        if (Physics.Raycast(lightFlattened, lightToPlayer, out hit, lt.range * .4f) && angle < (arc/2) && hit.transform == this.transform)  // checks to see if it is close enough to the player to actually cast light AND if it is within the arc of light 
+        {
+            Debug.DrawRay(lightFlattened, lightToPlayer, Color.green);  // draws debug ray
+            lt.color = Color.red;
             return true;
+        }
         else
+        {
+            lt.color = Color.white;
             return false;
+        }
     }
 
     bool isLitPoint(GameObject lite)    // checks to see if the light passed in as a parameter is lighting the player (assumes that it is a point light)
@@ -54,14 +69,22 @@ public class PlayerController : MonoBehaviour {
         Light lt = lite.GetComponent<Light>();  // grabs light component
         Vector3 lightToPlayer = this.transform.position - lite.GetComponent<Transform>().position;  // gets a vecctor from the light to the player
         Vector3 lightFlattened = lite.GetComponent<Transform>().position;   // gets position vector of the light
-        lightToPlayer.z = 0;    // flattens the forward vector on the z axis
-        lightFlattened.z = 0;   // flattens the vector to the player on the z axis
+        lightToPlayer.y = 0;    // flattens the forward vector on the y axis
+        lightFlattened.y = 0;   // flattens the vector to the player on the y axis
 
-        Debug.DrawRay(lightFlattened, lightToPlayer, Color.green);  // draws debug ray
-        if (Physics.Raycast(lightFlattened, lightToPlayer, lt.range * .7f)) // checks to see if it is close enough to the player to actually cast light
+        RaycastHit hit;
+
+        if (Physics.Raycast(lightFlattened, lightToPlayer, out hit, lt.range * .7f) && hit.transform == this.transform) // checks to see if it is close enough to the player to actually cast light
+        {
+            Debug.DrawRay(lightFlattened, lightToPlayer, Color.green);  // draws debug ray
+            lt.color = Color.red;
             return true;
+        }
         else
+        {
+            lt.color = Color.white;
             return false;
+        }
     }
 
     void processInput()
