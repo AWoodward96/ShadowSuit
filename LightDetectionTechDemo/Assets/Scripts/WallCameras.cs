c0cc0049a951878dc69207b0bfd0eb4f8272a11a
tyler13731 <tylerac@rochester.rr.com>
2016-11-28 00:14:06 -0500

    More work with cameras
    Submitting what I have. Still not happy with it, but reasonably works.
    Camera rotations still DO NOT work

--- LightDetectionTechDemo/Assets/Scripts/WallCameras.cs
+++ LightDetectionTechDemo/Assets/Scripts/WallCameras.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class WallCameras : MonoBehaviour
 {
@@ -15,10 +17,25 @@ public class WallCameras : MonoBehaviour
     int counter = 0;
 
     public int visionDetail = 200;
+    List<TexPoint> visionPath = new List<TexPoint>();
+    Texture2D blankTexture;
 
     // Use this for initialization
     void Start ()
     {
+        //basic blank texture to base the actual camera texture on
+        blankTexture = new Texture2D(visionDetail * range, visionDetail * range, TextureFormat.ARGB32, false);
+        for (int i = 0; i < visionDetail * range; i++)
+        {
+            for (int i2 = 0; i2 < visionDetail * range; i2++)
+            {
+                blankTexture.SetPixel(i, i2, Color.clear);
+            }
+        }
+        // Apply all SetPixel calls
+        blankTexture.Apply();
+
+        createVisionPath();
         createVision();
 
         if (rotateLeft == 0)
@@ -32,27 +49,29 @@ public class WallCameras : MonoBehaviour
             direcLeft = true;
         }
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        if(direcLeft && rotateLeft != angleDirection)
+        if (direcLeft && rotateLeft != angleDirection)
         {
-            angleDirection += .5f;
-            if(Mathf.Abs(rotateLeft - angleDirection) < 1)
+            //disabling rotation for now
+            //angleDirection += .5f;
+            if (Mathf.Abs(rotateLeft - angleDirection) < 1)
             {
                 direcLeft = false;
             }
         }
         if (!direcLeft && rotateRight != angleDirection)
         {
-            angleDirection -= .5f;
+            //disabling rotation for now
+            //angleDirection -= .5f;
             if (Mathf.Abs(rotateRight - angleDirection) < 1)
             {
                 direcLeft = true;
             }
         }
-        while(angleDirection > 360)
+        while (angleDirection > 360)
         {
             angleDirection -= 360;
         }
@@ -78,11 +97,11 @@ public class WallCameras : MonoBehaviour
 
         //attempting to control framerate issues in any way
         counter++;
-        if(counter == 5)
+        if (counter == 3)
         {
             counter = 0;
-            //createVision();
         }
+        createVision();
     }
 
     //THIS STILL CAUSES LOTS OF LAG
@@ -93,24 +112,72 @@ public class WallCameras : MonoBehaviour
 
         //scaled size of each pixel, inverted (200 is max size)
         int numb = visionDetail;
-        //scales the image to be the size it should be
-        gameObject.transform.GetChild(0).transform.localScale = new Vector3(200/numb, 200/numb, 0);
 
-        // Create a new 2x2 texture ARGB32 (32 bit with alpha) and no mipmaps
-        var texture = new Texture2D(numb * range, numb * range, TextureFormat.ARGB32, false);
+        float angleDetail = .25f;
 
-        // set the pixel values
-        for (int i = 0; i < texture.width; i++)
+        //creates a set of raycasts at incrementing angles. These angles are used to check sets of pixels instead of each pixel raycasting
+        float[] rayDist = new float[(int)(((angleDirection + angleRange) - (angleDirection - angleRange)) / angleDetail) + 1];
+        for (int i = 0; i <= ((angleDirection + angleRange) - (angleDirection - angleRange)) / angleDetail; i++)
         {
-            //the x point if the center is 0
-            int iFromCent = i - (texture.width/2);
-            for (int i2 = 0; i2 < texture.height; i2++)
+            if (Physics.Raycast(transform.position, new Vector3(-Mathf.Cos(Mathf.Deg2Rad * ((angleDirection - angleRange) + (i * angleDetail))), transform.position.y, -Mathf.Sin(Mathf.Deg2Rad * ((angleDirection - angleRange) + (i * angleDetail)))), out hit, range) && hit.collider == scene.GetComponent<Collider>())
+            { 
+                rayDist[i] = hit.distance;
+                //Debug.DrawLine(transform.position, transform.position - new Vector3(hit.distance * Mathf.Cos(Mathf.Deg2Rad * ((angleDirection - angleRange) + (i * angleDetail))), transform.position.y, hit.distance * Mathf.Sin(Mathf.Deg2Rad * ((angleDirection - angleRange) + (i * angleDetail)))), Color.green);  // draws debug ray
+            }
+            else
             {
-                texture.SetPixel(i, i2, Color.clear);
-                //the y point if the center is 0
-                int i2FromCent = i2 - (texture.height/2);
-                //makes sure the point is within a certain distance range to qualify
-                if (Mathf.Pow(iFromCent + iFromCent, 2) + Mathf.Pow(i2FromCent + i2FromCent, 2) < Mathf.Pow(numb * range, 2) && Mathf.Pow(iFromCent + iFromCent, 2) + Mathf.Pow(i2FromCent + i2FromCent, 2) > Mathf.Pow((numb/5) * range, 2))
+                rayDist[i] = 10;
+                //Debug.DrawLine(transform.position, transform.position - new Vector3(range * Mathf.Cos(Mathf.Deg2Rad * ((angleDirection - angleRange) + (i * angleDetail))), transform.position.y, range * Mathf.Sin(Mathf.Deg2Rad * ((angleDirection - angleRange) + (i * angleDetail)))), Color.green);  // draws debug ray
+            }
+        }
+
+        //scales the image to be the size it should be
+        gameObject.transform.GetChild(0).transform.localScale = new Vector3(200 / numb, 200 / numb, 0);
+
+        // Create a new texture ARGB32 (32 bit with alpha) and no mipmaps
+        var texture = new Texture2D(visionDetail * range, visionDetail * range, TextureFormat.ARGB32, false);
+        //var texture = blankTexture;
+        texture.SetPixels(blankTexture.GetPixels());
+        
+        //loops through the set pixels that are a part of the camera's view
+        for (int i = 0; i < visionPath.Count; i++)
+        {
+            float iFromCent = (visionPath[i].pos.x - (texture.width / 2));
+            float i2FromCent = (visionPath[i].pos.y - (texture.height / 2));
+            //for rotations whenever that happens
+            //float iFromCentRot = ((iFromCent * Mathf.Cos(Mathf.Deg2Rad * (90 - angleDirection))) - (i2FromCent * Mathf.Sin(Mathf.Deg2Rad * (90 - angleDirection))));
+            //float i2FromCentRot = ((i2FromCent * Mathf.Cos(Mathf.Deg2Rad * (90 - angleDirection))) + (iFromCent * Mathf.Sin(Mathf.Deg2Rad * (90 - angleDirection))));
+            int arrayCheck = (int)(Mathf.Round((visionPath[i].angle + (90 - angleDirection)) / angleDetail) - ((angleDirection - angleRange) / angleDetail));
+            if (new Vector2(iFromCent * 2, i2FromCent * 2).magnitude  < (numb * (rayDist[arrayCheck])))
+            {
+                //sets the individual pixel's color
+                texture.SetPixel((int)visionPath[i].pos.x, (int)visionPath[i].pos.y, Color.white);
+                //for rotations whenever that happens
+                //texture.SetPixel((int)(iFromCentRot + (texture.width / 2)), (int)(i2FromCentRot + (texture.height / 2)), Color.white);
+            }
+        }
+
+        // Apply all SetPixel calls
+        texture.Apply();
+
+        // connect texture to material of GameObject this script is attached to
+        gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+    }
+
+    //creates the list of pixels that are included in the camera texture
+    public void createVisionPath()
+    {
+        int numb = visionDetail;
+
+        //loops through every possible pixel, only adding the pixels included in the camera's range
+        for (int i = 0; i < numb * range; i++)
+        {
+            int iFromCent = i - (numb * range / 2);
+
+            for (int i2 = 0; i2 < numb * range; i2++)
+            {
+                int i2FromCent = i2 - (numb * range / 2);
+                if (Mathf.Pow(iFromCent + iFromCent, 2) + Mathf.Pow(i2FromCent + i2FromCent, 2) < Mathf.Pow(numb * range, 2) && Mathf.Pow(iFromCent + iFromCent, 2) + Mathf.Pow(i2FromCent + i2FromCent, 2) > Mathf.Pow((numb / 5) * range, 2))
                 {
                     float angle;
                     //turns the points into a vector
@@ -128,26 +195,11 @@ public class WallCameras : MonoBehaviour
                     //checks to see if the point is within the angle range
                     if (angle >= angleDirection - angleRange && angle <= angleDirection + angleRange)
                     {
-                        //scaling number based on how detailed the vision cone is
-                        int divideBy = numb / 2;
-                        //checks a raycast for each point of the cone, not drawing the pixel if the walls of the scene are in the way
-                        if (Physics.Raycast(transform.position, (new Vector3(iFromCent, 0, -i2FromCent) / divideBy), out hit, new Vector3(iFromCent, 0, -i2FromCent).magnitude / divideBy) && hit.collider == scene.GetComponent<Collider>())
-                        {
-                        }
-                        else
-                        {
-                            texture.SetPixel(i, i2, Color.white);
-                        }
+                        visionPath.Add(new TexPoint(i, i2, angle));
                     }
                 }
             }
         }
-
-        // Apply all SetPixel calls
-        texture.Apply();
-
-        // connect texture to material of GameObject this script is attached to
-        gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
     }
 
     public bool CanSee(GameObject target)
@@ -185,5 +237,17 @@ public class WallCameras : MonoBehaviour
             return true;
         }
         return false;
+    }
+}
+
+public class TexPoint
+{
+    public Vector2 pos;
+    public float angle;
+
+    public TexPoint(int x, int y, float a)
+    {
+        pos = new Vector2(x, y);
+        angle = a;
     }
 }
